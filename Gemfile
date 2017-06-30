b446b067880063c69d57669770fa737574eb427b
Laurent Milne <lmilne360@gmail.com>
2017-06-30 03:46:51 -0400

    Added search functionality

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'bootstrap-sass'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'pry', '~> 0.10.4'
 end
 
 group :development do
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,14 @@ class StaticPagesController < ApplicationController
   def favorites
   end
 
+  def search
+    details = Gems.search(params["search-term"])
+    if details.first["name"] == params['search-term']
+      @gem = details.first
+    else
+      @gem = nil
+    end
+    render :root
+  end
+
 end
