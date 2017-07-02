3e6d340cb1d1b2158788f6ca7184ae8c9e819b19
Laurent Milne <lmilne360@gmail.com>
2017-07-02 17:17:56 -0400

    created catcher for invalid routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
   get '/favorites' => "static_pages#favorites"
   post '/search' => "static_pages#search"
+
+  get '*path' => "static_pages#root"
 end
